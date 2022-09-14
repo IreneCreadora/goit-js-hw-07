c2a38d1a770e152e1e8a8dcf18ba407395349848
IreneCreadora <nargiza.dancer@gmail.com>
2022-09-14 15:16:31 +0300

    create lightBox gallery of images

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryContainer: document.querySelector('.gallery'),
+};
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+refs.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryCardsMarkup(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    })
+    .join('');
+}
+var lightBox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
